68a7b90602f62e7cae60f6833c38b09833937912
tius-su <tiuss168@gmail.com>
2026-04-20 11:03:44 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'tius-pwa-v1';
+const assets = [
+  'index.html',
+  'logo.jpg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
